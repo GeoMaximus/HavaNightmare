39237188edb0d13388524ca5e114f7d1c4cdf463
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-03-31 15:45:07 +0300

    another one

--- seminar03/src/main/java/Main.java
+++ seminar03/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import seminar03.Car;
 import seminar03.Taxable;
 import seminar03.Vehicle;
 
@@ -5,5 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Taxable t;
         Vehicle v;
+
+        Car c1 = new Car("Dacia Logan", 100, 1000);
+        c1.move();
+        System.out.println(c1.computeTax());
     }
 }
--- seminar03/src/main/java/seminar03/Car.java
+++ seminar03/src/main/java/seminar03/Car.java
@@ -1,16 +1,33 @@
 package seminar03;
 
-public class Car extends Vehicle implements Taxable{
+public final class Car extends Vehicle implements Taxable{
+    private double engineSize;
+    private final double ENGINE_LIMIT = 2000;
+    public Car(){
+        this.engineSize = 0;
+    }
 
-    
+    public Car(String name, int speed, double engineSize) {
+        super(name, speed);
+        this.engineSize = engineSize;
+    }
+
+    public Car(double engineSize) {
+        this.engineSize = engineSize;
+    }
 
     @Override
     public void move() {
-
+        System.out.println("The car " + getName() + "is moving with speed "  + getSpeed());
     }
 
     @Override
     public double computeTax() {
-        return 0;
+        if(ENGINE_LIMIT < 2){
+            return engineSize*0.05;
+        }
+        else {
+            return engineSize*0.1;
+        }
     }
 }
